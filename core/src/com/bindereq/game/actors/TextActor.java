488c47000667b3044ffb06550f32d0aa53f4045c
OrthodoxEgo <vgtrofimov@gmail.com>
2022-03-30 17:55:33 +0300

    Работа со шрифтами.

--- core/src/com/bindereq/game/actors/TextActor.java
+++ core/src/com/bindereq/game/actors/TextActor.java
@@ -13,6 +13,8 @@ public class TextActor extends Actor {
     BitmapFont font;
     String textLine, color;
     int widthText, heightText;
+    int[] timeAlpha = new int[] {1, 3, 5};
+    float time = 0.0f, alpha = 0.0f;
 
 
     public TextActor(float x, float y, String textLine, BitmapFont bitmapFont, GlyphLayout glyphLayout, String color) {
@@ -34,12 +36,24 @@ public class TextActor extends Actor {
     @Override
     public void act(float delta) {
         super.act(delta);
+        time += delta;
+        if (time < timeAlpha[0]) {
+            alpha += 0.1f;
+        } else if (time < timeAlpha[1]) {
+
+        } else if (time < timeAlpha[2]) {
+            alpha -= 0.1f;
+
+        }
+
+        if (alpha > 1.0f) alpha = 1.0f;
+        else if (alpha < 0.0f) alpha = 0.0f;
 
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        super.draw(batch, parentAlpha);
+        super.draw(batch, 1);
         font.setColor(Color.valueOf(color));
         font.draw(batch, textLine, getX(), getY());
     }
